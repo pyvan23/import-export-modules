c256c36c9f7b42f4a9f63be1ffd8b734183c60e7
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-10-14 18:06:45 -0300

    packagejson

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 import { multiply } from "./helpers/multiply.js";
-import { hideBin } from 'yargs/helpers'
-import yargs from 'yargs'
+import { hideBin } from "yargs/helpers";
+import yargs from "yargs";
 
 console.clear();
-const argv = yargs(hideBin(process.argv)).options('b',{
-  alias:'base',
-  type:'number'
-})
-.argv
+const argv = yargs(hideBin(process.argv))
+.options("b", {
+  alias: "base",
+  type: "number",
+}).argv;
+
 //feedback errors
 multiply(4)
-.then((file) => console.log(file, 'created'))
-.catch((err) => console.log(err))
+  .then((file) => console.log(file, "created"))
+  .catch((err) => console.log(err));
 
-console.log(process.argv)
+console.log(process.argv);
 console.log(argv);
