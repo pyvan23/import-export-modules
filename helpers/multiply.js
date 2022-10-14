ccc143f0a88d554ef8687bb657b8a49348650ae2
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-10-14 00:03:44 -0300

    colors package

--- helpers/multiply.js
+++ helpers/multiply.js
@@ -1,10 +1,17 @@
 import { writeFileSync } from "node:fs";
+import Color from "colors";
+
+
+
 
 export const multiply = async (number = 5) => {
-  try {
+    try {
+        Color.setTheme({
+            custom: ['red', 'underline']
+          });
 
     console.log("**************");
-    console.log(" T A B L E S ");
+    console.log(Color.cyan(" T A B L E S "));
     console.log("**************");
 
     let out = "";
